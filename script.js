2e9e934acfae2f1638c01f9b197ed469b87e784c
Shaira <132321217+luvshr@users.noreply.github.com>
2023-09-08 14:47:07 +0800

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 let navbar = document.querySelector(".navbar");
 
-document.querySelector("#menu-btn").onclick = () => {
+document.querySelector("#travels-btn").onclick = () => {
   navbar.classList.toggle("active");
   searchForm.classList.remove("active");
   cartItem.classList.remove("active");
-};
-
-let searchForm = document.querySelector(".search-form");
-
-document.querySelector("#search-btn").onclick = () => {
-  searchForm.classList.toggle("active");
-  navbar.classList.remove("active");
-  cartItem.classList.remove("active");
-};
-
-let cartItem = document.querySelector(".cart-items-container");
-
-document.querySelector("#cart-btn").onclick = () => {
-  cartItem.classList.toggle("active");
-  searchForm.classList.remove("active");
-  navbar.classList.remove("active");
 };
 
 window.onscroll = () => {
