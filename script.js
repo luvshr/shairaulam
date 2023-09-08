fe981eddfdfd5ef522d1dd6942d76f4a2bcd4421
Shaira <132321217+luvshr@users.noreply.github.com>
2023-09-08 14:48:10 +0800

    script.js

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ document.querySelector("#travels-btn").onclick = () => {
   cartItem.classList.remove("active");
 };
 
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let cartItem = document.querySelector(".cart-items-container");
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  searchForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
 window.onscroll = () => {
   navbar.classList.remove("active");
   searchForm.classList.remove("active");
